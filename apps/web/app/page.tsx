528178552fe748841b9a066d44326e17529f1ea2
Quoc Dao <quoc01224029840@gmail.com>
2026-01-04 22:34:21 +0700

    chore: Force dynamic rendering for the home page.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,8 @@ import { Hero } from './components/hero-section';
 
 import { FeaturedCollections } from './components/featured-collection';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className='relative min-h-screen bg-white'>
